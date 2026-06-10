6b1e684bf8fe01a8c363bc18dee3017e83d062fd
Amanda Williams-Newkirk <igy7@cdc.gov>
2026-06-10 13:56:22 -0400

    Troubleshooting missing file at the end of the run. All data processing runs as expected.

--- test_data/test_pipeline_ci.sh
+++ test_data/test_pipeline_ci.sh
@@ -2,6 +2,8 @@
 
 cd ..
 nextflow run hmas2.nf -profile test,git_action,singularity
+echo "Looking for output in: $(pwd)"
+find . -maxdepth 1 -type d | sort
 NXF_EXIT=$?
 
 if [ $NXF_EXIT -ne 0 ]; then
@@ -9,6 +11,8 @@ if [ $NXF_EXIT -ne 0 ]; then
   exit 1
 fi
 
+echo "Looking for output in: $(pwd)"
+find . -maxdepth 1 -type d | sort
 latest_testoutput=$(find . -maxdepth 1 -type d -name 'test_output*' | sort -r | head -n 1)
 
 if [ -z "$latest_testoutput" ]; then
@@ -19,6 +23,8 @@ fi
 generated_csv="$latest_testoutput/report_sorted.csv"
 expected_csv="test_data/report_ref_sorted.csv"
 
+echo "Looking for output in: $(pwd)"
+find . -maxdepth 1 -type d | sort
 report_file=$(find "$latest_testoutput" -type f -name 'report*.csv')
 
 if [ -z "$report_file" ]; then
